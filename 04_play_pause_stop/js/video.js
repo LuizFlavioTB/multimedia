db19d2de39aa976e7d1ab418fac886d5e522d8a9
Luiz Flavio <luizflaviotb@gmail.com>
2021-11-13 17:32:39 -0400

    insert button - play, pause and stop

---
+++ 04_play_pause_stop/js/video.js
@@ -0,0 +1,19 @@
+function getVideo() {
+  return document.getElementById("video");
+}
+
+function play () {
+  var video = getVideo();
+  video.play(); 
+}
+
+function pause () {
+  var video = getVideo();
+  video.pause(); 
+}
+
+function stop () {
+  var video = getVideo();
+  video.load();
+
+}
